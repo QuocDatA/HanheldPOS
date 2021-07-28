c0f2e6078f329d835f9fb363a21c6ae7fb7d0cac
Hưng Nguyễn Xuân <nxhung16102001@gmail.com>
2021-07-28 20:04:42 +0700

    Fix size ui welcome, device code, pin code

--- app/src/main/java/com/hanheldpos/utils/binding/BindingAdapterUtils.java
+++ app/src/main/java/com/hanheldpos/utils/binding/BindingAdapterUtils.java
@@ -61,14 +61,14 @@ public class BindingAdapterUtils {
             class SpaceSpan extends ReplacementSpan {
                 @Override
                 public int getSize(@NonNull Paint paint, CharSequence text, int start, int end, @Nullable Paint.FontMetricsInt fm) {
-                    float padding = paint.measureText("  ", 0, 2);
+                    float padding = paint.measureText(" ", 0, 1);
                     float textSize = paint.measureText(text, start, end);
                     return (int) (padding + textSize);
                 }
 
                 @Override
                 public void draw(@NonNull Canvas canvas, CharSequence text, int start, int end, float x, int top, int y, int bottom, @NonNull Paint paint) {
-                    canvas.drawText(text.subSequence(start, end) + "  ", x, y, paint);
+                    canvas.drawText(text.subSequence(start, end) + " ", x, y, paint);
                 }
             }
         });
