ea462fd3ce4029add75d4d2dfdd7bc21218fab65
KSB-tqk <linh05602@gmail.com>
2021-12-07 16:46:35 +0700

    [DEV] - Add New Customer

---
+++ app/src/main/java/com/hanheldpos/ui/screens/cart/customer/AddNewCustomerFragment.kt
@@ -0,0 +1,63 @@
+package com.hanheldpos.ui.screens.cart.customer
+
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
+import android.view.View
+import com.hanheldpos.R
+import com.hanheldpos.databinding.FragmentAddNewCustomerBinding
+import com.hanheldpos.ui.base.fragment.BaseFragment
+import java.util.*
+import javax.xml.datatype.DatatypeConstants.MONTHS
+
+class AddNewCustomerFragment : BaseFragment<FragmentAddNewCustomerBinding, AddNewCustomerVM>(),
+    AddNewCustomerUV {
+    override fun layoutRes(): Int = R.layout.fragment_add_new_customer
+
+    override fun viewModelClass(): Class<AddNewCustomerVM> {
+        return AddNewCustomerVM::class.java;
+    }
+
+    override fun initViewModel(viewModel: AddNewCustomerVM) {
+        viewModel.run {
+            init(this@AddNewCustomerFragment);
+            binding.viewModel = this
+        }
+        binding.birthDayTextInputLayout.setEndIconOnClickListener(View.OnClickListener {
+            onDateTimePick()
+        })
+    }
+
+    override fun initView() {
+
+    }
+
+    override fun initData() {
+
+    }
+
+    override fun initAction() {
+
+    }
+
+    override fun getBack() {
+        onFragmentBackPressed()
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onDateTimePick() {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(this.requireActivity(), DatePickerDialog.OnDateSetListener { view, yearOfPick, monthOfPick, dayOfPick ->
+
+            // Display Selected date in textbox
+            val monthPicked = monthOfPick+1
+            binding.birthDayInput.setText("$dayOfPick/$monthPicked/$yearOfPick")
+
+        }, year, month, day)
+
+        datePickerDialog.show()
+    }
+}
