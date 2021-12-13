69a7520f8d3ef273379146da2c18b39eac93aa7e
KSB-tqk <linh05602@gmail.com>
2021-12-13 18:20:30 +0700

    [DEV] - Add BaseProductInCart List to Discount Fragment

--- app/src/main/java/com/hanheldpos/ui/screens/discount/discounttype/DiscountTypeFragment.kt
+++ app/src/main/java/com/hanheldpos/ui/screens/discount/discounttype/DiscountTypeFragment.kt
@@ -1,6 +1,9 @@
 package com.hanheldpos.ui.screens.discount.discounttype
 
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.hanheldpos.R
 import com.hanheldpos.data.api.pojo.order.settings.Reason
 import com.hanheldpos.databinding.FragmentDiscountTypeBinding
@@ -9,13 +12,16 @@ import com.hanheldpos.model.discount.DiscountApplyToType
 import com.hanheldpos.model.discount.DiscountTypeFor
 import com.hanheldpos.model.discount.DiscountTypeTab
 import com.hanheldpos.model.discount.DiscountUser
+import com.hanheldpos.model.product.BaseProductInCart
 import com.hanheldpos.ui.base.adapter.BaseItemClickListener
 import com.hanheldpos.ui.base.fragment.BaseFragment
+import com.hanheldpos.ui.screens.discount.discounttype.adapter.DiscountItemAdapter
 import com.hanheldpos.ui.screens.discount.discounttype.adapter.DiscountTabAdapter
 import com.hanheldpos.ui.screens.discount.discounttype.amount.DiscountAmountFragment
 import com.hanheldpos.ui.screens.discount.discounttype.comp.DiscountCompFragment
 import com.hanheldpos.ui.screens.discount.discounttype.discount_code.DiscountCodeFragment
 import com.hanheldpos.ui.screens.discount.discounttype.percentage.DiscountPercentageFragment
+import com.hanheldpos.ui.screens.product.adapter.GridSpacingItemDecoration
 import com.hanheldpos.ui.screens.product.adapter.OptionsPagerAdapter
 
 class DiscountTypeFragment(private val applyToType: DiscountApplyToType, private val cart : CartModel, private val listener : DiscountTypeListener) :
@@ -24,6 +30,7 @@ class DiscountTypeFragment(private val applyToType: DiscountApplyToType, private
     // Adapter
     private lateinit var adapter: DiscountTabAdapter;
     private lateinit var optionsPagerAdapter: OptionsPagerAdapter;
+    private lateinit var itemDiscountAdapter: DiscountItemAdapter;
 
 
 
@@ -57,13 +64,39 @@ class DiscountTypeFragment(private val applyToType: DiscountApplyToType, private
             setHasFixedSize(true);
         }
 
+        //Discount Item Adapter
+        itemDiscountAdapter =
+            DiscountItemAdapter(listener = object : BaseItemClickListener<BaseProductInCart> {
+                override fun onItemClick(adapterPosition: Int, item: BaseProductInCart) {
+
+                }
+            })
+        binding.itemDiscountContainer.apply {
+            addItemDecoration(
+                DividerItemDecoration(
+                    context,
+                    LinearLayoutManager.VERTICAL
+                ).apply {
+                    setDrawable(
+                        ContextCompat.getDrawable(
+                            context,
+                            R.drawable.divider_vertical
+                        )!!
+                    )
+                }
+            )
+        }
+        binding.itemDiscountContainer.adapter = itemDiscountAdapter
+
         // Container Fragment Type For Adapter
-        optionsPagerAdapter = OptionsPagerAdapter(childFragmentManager,lifecycle);
+        optionsPagerAdapter = OptionsPagerAdapter(childFragmentManager, lifecycle);
         binding.discountFragmentContainer.adapter = optionsPagerAdapter;
 
     }
 
     override fun initData() {
+        binding.applyToType = this.applyToType
+
         // Data Discount Tab Adapter
         val listTab = mutableListOf(
             DiscountTypeTab(title = "Amount (đ)", type = DiscountTypeFor.AMOUNT),
@@ -77,15 +110,21 @@ class DiscountTypeFragment(private val applyToType: DiscountApplyToType, private
             )
         adapter.submitList(listTab);
 
+        // Data Discount Item Adapter
+        itemDiscountAdapter.submitList(cart.productsList)
+        val theName = cart.productsList[0].name
+
         // Data Container Fragment Type
-        fragmentMap[DiscountTypeFor.AMOUNT] = DiscountAmountFragment(listener = object : DiscountTypeListener {
-            override fun discountUserChoose(discount: DiscountUser) {
-                listener.discountUserChoose(discount);
-            }
-        });
-        fragmentMap[DiscountTypeFor.PERCENTAGE] = DiscountPercentageFragment(listener = object : DiscountTypeListener {
-            override fun discountUserChoose(discount: DiscountUser) {
-                listener.discountUserChoose(discount);
+        fragmentMap[DiscountTypeFor.AMOUNT] =
+            DiscountAmountFragment(listener = object : DiscountTypeListener {
+                override fun discountUserChoose(discount: DiscountUser) {
+                    listener.discountUserChoose(discount);
+                }
+            });
+        fragmentMap[DiscountTypeFor.PERCENTAGE] =
+            DiscountPercentageFragment(listener = object : DiscountTypeListener {
+                override fun discountUserChoose(discount: DiscountUser) {
+                    listener.discountUserChoose(discount);
             }
         });
         fragmentMap[DiscountTypeFor.DISCOUNT_CODE] = DiscountCodeFragment(applyToType);
---
+++ app/src/main/java/com/hanheldpos/ui/screens/discount/discounttype/adapter/DiscountItemAdapter.kt
@@ -0,0 +1,51 @@
+package com.hanheldpos.ui.screens.discount.discounttype.adapter
+
+import androidx.recyclerview.widget.DiffUtil
+import com.hanheldpos.R
+import com.hanheldpos.databinding.ItemDiscountBinding
+import com.hanheldpos.databinding.ItemDiscountCompBinding
+import com.hanheldpos.model.product.BaseProductInCart
+import com.hanheldpos.ui.base.adapter.BaseBindingListAdapter
+import com.hanheldpos.ui.base.adapter.BaseBindingViewHolder
+import com.hanheldpos.ui.base.adapter.BaseItemClickListener
+
+class DiscountItemAdapter(private val listener: BaseItemClickListener<BaseProductInCart>) :
+    BaseBindingListAdapter<BaseProductInCart>(DiffCallback()) {
+
+    data class SelectedItem(var value: Int = -1)
+
+    private var selectedItem: SelectedItem = SelectedItem(-1)
+
+    override fun getItemViewType(position: Int): Int {
+        return R.layout.item_discount;
+    }
+
+    override fun onBindViewHolder(holder: BaseBindingViewHolder<BaseProductInCart>, position: Int) {
+        val item = getItem(position);
+        val binding = holder.binding as ItemDiscountBinding;
+        binding.item = item;
+        binding.btnProductItem.apply {
+            setOnClickListener {
+                notifyItemChanged(selectedItem.value);
+                selectedItem.value = position;
+                notifyItemChanged(position);
+                listener.onItemClick(position, item);
+            }
+            isChecked = selectedItem.value == position;
+        }
+    }
+
+    class DiffCallback : DiffUtil.ItemCallback<BaseProductInCart>() {
+        override fun areItemsTheSame(oldItem: BaseProductInCart, newItem: BaseProductInCart): Boolean {
+            return oldItem == newItem;
+        }
+
+        override fun areContentsTheSame(
+            oldItem: BaseProductInCart,
+            newItem: BaseProductInCart
+        ): Boolean {
+            return false
+        }
+
+    }
+}
