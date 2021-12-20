7b558a5c31a06b933d1cfba8d64458f81917d9a1
Xuan Hung <nxhung16102001@gmail.com>
2021-12-20 14:38:23 +0700

    fix - load more customer

--- app/src/main/java/com/hanheldpos/ui/screens/cart/customer/adapter/CustomerAdapterHelper.kt
+++ app/src/main/java/com/hanheldpos/ui/screens/cart/customer/adapter/CustomerAdapterHelper.kt
@@ -1,10 +1,12 @@
 package com.hanheldpos.ui.screens.cart.customer.adapter
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.MutableLiveData
 import com.hanheldpos.data.api.pojo.customer.CustomerResp
 
-class CustomerAdapterHelper(private val listener : SearchCallBack ) {
-    private val listCustomer : MutableList<CustomerResp?> = mutableListOf();
+class CustomerAdapterHelper(private val listener: SearchCallBack) {
+    private val listCustomer: MutableList<CustomerResp?> = mutableListOf();
     private var isLoadingMore = false;
     private var currentPage = 1;
     private var currentKeyword = "";
@@ -17,32 +19,32 @@ class CustomerAdapterHelper(private val listener : SearchCallBack ) {
         if (isLoadingMore || currentKeyword.trim().isBlank()) return;
         isLoadingMore = true;
         currentPage = currentPage.plus(1);
-        listener.onSearch(currentKeyword,currentPage);
+        listener.onSearch(currentKeyword, currentPage);
         listCustomer.add(null);
         listener.onLoadingNextPage(listCustomer);
     }
 
-    fun search(keyword : String) {
+    fun search(keyword: String) {
         init();
         currentKeyword = keyword;
         listener.onSearch(keyword);
     }
 
-    fun loadedSuccess(customers : List<CustomerResp>){
-        if (listCustomer.isNotEmpty())
-        listCustomer.removeAt(listCustomer.size-1);
-        listener.onLoadedNextPage(listCustomer.size, listCustomer.apply { addAll(customers) })
-        isLoadingMore = false;
-    }
 
-    fun loadedFail(){
-        currentPage = currentPage.minus(1);
+    fun loaded(customers: List<CustomerResp>, isSuccess: Boolean) {
+        listCustomer.removeAll { it == null };
+        if (isSuccess)
+            listener.onLoadedNextPage(if (listCustomer.isEmpty()) 0 else listCustomer.size-1 , listCustomer.apply { addAll(customers) })
+        else {
+            currentPage = currentPage.minus(1);
+            listener.onLoadedNextPage(if (listCustomer.isEmpty()) 0 else listCustomer.size-1, listCustomer);
+        }
         isLoadingMore = false;
     }
 
     interface SearchCallBack {
-        fun onSearch(keyword : String,pageNo : Int? = 1);
-        fun onLoadingNextPage(customers : List<CustomerResp?>);
-        fun onLoadedNextPage(startIndex : Int,customers : List<CustomerResp?>);
+        fun onSearch(keyword: String, pageNo: Int? = 1);
+        fun onLoadingNextPage(customers: List<CustomerResp?>);
+        fun onLoadedNextPage(startIndex: Int, customers: List<CustomerResp?>);
     }
 }
