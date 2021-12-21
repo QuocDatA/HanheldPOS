a1ff0504d7cd799a54a5ee6dcc4a5ae4bc498d6c
KSB-tqk <linh05602@gmail.com>
2021-12-21 10:46:55 +0700

    [REFACTOR] - REFACTOR TableAdapter and Table UI

--- app/src/main/java/com/hanheldpos/ui/screens/home/table/adapter/TableAdapter.kt
+++ app/src/main/java/com/hanheldpos/ui/screens/home/table/adapter/TableAdapter.kt
@@ -1,6 +1,11 @@
 package com.hanheldpos.ui.screens.home.table.adapter
 
+import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.graphics.Color.TRANSPARENT
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import androidx.databinding.DataBindingUtil
@@ -9,7 +14,10 @@ import androidx.recyclerview.widget.DiffUtil
 import com.hanheldpos.R
 import com.hanheldpos.data.api.pojo.table.FloorTableItem
 import com.hanheldpos.databinding.ItemTableBinding
+import com.hanheldpos.databinding.ItemTableDirectionButtonBinding
+import com.hanheldpos.databinding.ItemTableEmptyBinding
 import com.hanheldpos.model.home.table.TableModeViewType
+import com.hanheldpos.model.home.table.TableStatusType
 import com.hanheldpos.ui.base.adapter.BaseBindingListAdapter
 import com.hanheldpos.ui.base.adapter.BaseBindingViewHolder
 import com.hanheldpos.ui.base.adapter.BaseItemClickListener
@@ -18,8 +26,16 @@ class TableAdapter(
     private val listener: BaseItemClickListener<FloorTableItem>
 ) : BaseBindingListAdapter<FloorTableItem>(DiffCallBack(),listener) {
 
+    lateinit var context: Context
+
     override fun getItemViewType(position: Int): Int {
-        return R.layout.item_table;
+        return when (getItem(position).uiType) {
+            TableModeViewType.Table -> R.layout.item_table
+            TableModeViewType.Empty -> R.layout.item_table_empty
+            else -> {
+                R.layout.item_table_direction_button
+            }
+        }
     }
 
     override fun onCreateViewHolder(
@@ -32,22 +48,116 @@ class TableAdapter(
             parent, false
         ).also {
             //Log.d("TableAdapter","RecycleView Height:" + parent.height);
-            val height = ((parent.height) / 5 ) - parent.resources.getDimension(R.dimen._6sdp);
-            val params : FrameLayout.LayoutParams = FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,height.toInt());
-            (it as ItemTableBinding).layoutMain.layoutParams = params;
+            val height = ((parent.height) / 5) - parent.resources.getDimension(R.dimen._6sdp);
+            val params: FrameLayout.LayoutParams =
+                FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, height.toInt());
+            when (viewType) {
+                R.layout.item_table -> (it as ItemTableBinding).layoutMain.layoutParams = params;
+                R.layout.item_table_direction_button -> (it as ItemTableDirectionButtonBinding).layoutMain.layoutParams =
+                    params
+                else -> {
+                    (it as ItemTableEmptyBinding).layoutMain.layoutParams = params
+                }
+            }
+            this.context = parent.context
+
             return BaseBindingViewHolder(it, listener)
         }
     }
 
+    override fun onBindViewHolder(holder: BaseBindingViewHolder<FloorTableItem>, position: Int) {
+        val item = getItem(position);
 
+        //Color for the Relative Layout Background
+        val availableColor = Color.parseColor("#EEEEEE")
+        val pendingColor = Color.parseColor("#007EFF")
+        val unavailableColor = Color.parseColor("#FBD536")
+        if (item.uiType != TableModeViewType.Empty) {
+            when (item.tableStatus) {
+                TableStatusType.Available -> {
+                    when (item.uiType) {
+                        TableModeViewType.Table -> {
+                            val view = holder.binding as ItemTableBinding
+                            view.tableItemBackground.setBackgroundColor(
+                                availableColor
+                            )
+                        }
+                        else -> {
+                            val dirButtonView = holder.binding as ItemTableDirectionButtonBinding
+                            dirButtonView.tableItemBackground.setBackgroundColor(
+                                availableColor
+                            )
+                        }
+                    }
+                }
+                TableStatusType.Pending -> {
+                    when (item.uiType) {
+                        TableModeViewType.Table -> {
+                            val view = holder.binding as ItemTableBinding
+                            view.tableItemBackground.setBackgroundColor(
+                                pendingColor
+                            )
+                        }
+                        else -> {
+                            val dirButtonView = holder.binding as ItemTableDirectionButtonBinding
+                            dirButtonView.tableItemBackground.setBackgroundColor(
+                                pendingColor
+                            )
+                        }
+                    }
+                }
+                TableStatusType.Unavailable -> {
+                    when (item.uiType) {
+                        TableModeViewType.Table -> {
+                            val view = holder.binding as ItemTableBinding
+                            view.tableItemBackground.setBackgroundColor(
+                               unavailableColor
+                            )
+                        }
+                        else -> {
+                            val dirButtonView = holder.binding as ItemTableDirectionButtonBinding
+                            dirButtonView.tableItemBackground.setBackgroundColor(
+                                unavailableColor
+                            )
+                        }
+                    }
+                }
+            }
+        }
 
-    private class DiffCallBack : DiffUtil.ItemCallback<FloorTableItem>(){
+        //Set Image For the DirButton
+        when (item.uiType) {
+            TableModeViewType.NextButtonEnable -> {
+                val view = holder.binding as ItemTableDirectionButtonBinding
+                view.dirButton.setImageResource(R.drawable.ic_direction_down)
+                view.dirButtonBackground.setRippleColorResource(R.color.color_2)
+            }
+            TableModeViewType.PrevButtonEnable -> {
+                val view = holder.binding as ItemTableDirectionButtonBinding
+                view.dirButton.setImageResource(R.drawable.ic_direction_up)
+                view.dirButtonBackground.setRippleColorResource(R.color.color_2)
+            }
+            TableModeViewType.NextButtonDisable -> {
+                val view = holder.binding as ItemTableDirectionButtonBinding
+                view.dirButton.setImageResource(R.drawable.ic_direction_down_disable)
+                view.dirButtonBackground.rippleColor = ColorStateList.valueOf(TRANSPARENT)
+            }
+            TableModeViewType.PrevButtonDisable -> {
+                val view = holder.binding as ItemTableDirectionButtonBinding
+                view.dirButton.setImageResource(R.drawable.ic_direction_up_disable)
+                view.dirButtonBackground.rippleColor = ColorStateList.valueOf(TRANSPARENT)
+            }
+        }
+        holder.bindItem(item);
+    }
+
+    private class DiffCallBack : DiffUtil.ItemCallback<FloorTableItem>() {
         override fun areItemsTheSame(oldItem: FloorTableItem, newItem: FloorTableItem): Boolean {
             return oldItem.id.equals(newItem.id) && ((oldItem.uiType == newItem.uiType) || (oldItem.uiType != TableModeViewType.Table && oldItem.uiType != TableModeViewType.Empty && newItem.uiType != TableModeViewType.Table && newItem.uiType != TableModeViewType.Empty))
         }
 
         override fun areContentsTheSame(oldItem: FloorTableItem, newItem: FloorTableItem): Boolean {
-            return false
+            return oldItem == newItem && (oldItem.uiType == newItem.uiType)
         }
 
     }
