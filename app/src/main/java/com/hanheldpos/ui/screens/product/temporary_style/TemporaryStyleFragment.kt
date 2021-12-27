1d9b1e537e897a2af5ee32b2770fb711caccc215
Xuan Hung <nxhung16102001@gmail.com>
2021-12-27 11:37:19 +0700

    update - ui style for variant

--- app/src/main/java/com/hanheldpos/ui/screens/product/temporary_style/TemporaryStyleFragment.kt
+++ app/src/main/java/com/hanheldpos/ui/screens/product/temporary_style/TemporaryStyleFragment.kt
@@ -8,8 +8,14 @@ import android.view.ViewGroup
 import com.hanheldpos.R
 import com.hanheldpos.databinding.FragmentTemporaryStyleBinding
 import com.hanheldpos.ui.base.fragment.BaseFragment
+import com.hanheldpos.ui.screens.product.adapter.GridSpacingItemDecoration
+import com.hanheldpos.ui.screens.product.temporary_style.adapter.TemporaryVariantAdapter
 
-class TemporaryStyleFragment : BaseFragment<FragmentTemporaryStyleBinding, TemporaryStyleVM>(), TemporaryStyleUV {
+class TemporaryStyleFragment : BaseFragment<FragmentTemporaryStyleBinding, TemporaryStyleVM>(),
+    TemporaryStyleUV {
+
+    private lateinit var variantAdapter: TemporaryVariantAdapter;
+
     override fun layoutRes(): Int {
         return R.layout.fragment_temporary_style;
     }
@@ -26,11 +32,16 @@ class TemporaryStyleFragment : BaseFragment<FragmentTemporaryStyleBinding, Tempo
     }
 
     override fun initView() {
-
+        variantAdapter = TemporaryVariantAdapter()
+        binding.containerVariant.apply {
+            adapter = variantAdapter; addItemDecoration(
+            GridSpacingItemDecoration(4, resources.getDimensionPixelSize(R.dimen._15sdp), false)
+        );
+        }
     }
 
     override fun initData() {
-
+        variantAdapter.submitList(mutableListOf(0, 1, 2, 3, 4,6,7,8));
     }
 
     override fun initAction() {
