cfe0f3ddad3b252f899a5510f4b0a196618956df
KSB-tqk <linh05602@gmail.com>
2021-12-27 11:50:03 +0700

    [DEV] - Add Temporary Sauce Item

--- app/src/main/java/com/hanheldpos/ui/screens/product/temporary_style/TemporaryStyleFragment.kt
+++ app/src/main/java/com/hanheldpos/ui/screens/product/temporary_style/TemporaryStyleFragment.kt
@@ -1,15 +1,20 @@
 package com.hanheldpos.ui.screens.product.temporary_style
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.hanheldpos.R
+import com.hanheldpos.data.api.pojo.payment.PaymentSuggestionItem
 import com.hanheldpos.databinding.FragmentTemporaryStyleBinding
+import com.hanheldpos.ui.base.adapter.BaseItemClickListener
 import com.hanheldpos.ui.base.fragment.BaseFragment
+import com.hanheldpos.ui.screens.product.adapter.GridSpacingItemDecoration
+import com.hanheldpos.ui.screens.product.temporary_style.temporary_adapter.TemporarySauceAdapter
 
 class TemporaryStyleFragment : BaseFragment<FragmentTemporaryStyleBinding, TemporaryStyleVM>(), TemporaryStyleUV {
+
+    private lateinit var temporarySauceAdapter: TemporarySauceAdapter
+
+    private val temporaryStyleVM by activityViewModels<TemporaryStyleVM>();
+
     override fun layoutRes(): Int {
         return R.layout.fragment_temporary_style;
     }
@@ -26,16 +31,43 @@ class TemporaryStyleFragment : BaseFragment<FragmentTemporaryStyleBinding, Tempo
     }
 
     override fun initView() {
-
+        //region setup sauce recycler view
+        temporarySauceAdapter = TemporarySauceAdapter(
+            onTemporarySauceItemClickListener = object : BaseItemClickListener<TemporarySauceItem> {
+                override fun onItemClick(adapterPosition: Int, item: TemporarySauceItem) {
+                }
+            },
+        );
+        binding.temporarySauceContainer.apply {
+            addItemDecoration(
+                GridSpacingItemDecoration(
+                    spanCount = 1,
+                    includeEdge = false,
+                    spacing = resources.getDimensionPixelSize(R.dimen._10sdp)
+                )
+            )
+            binding.temporarySauceContainer.adapter = temporarySauceAdapter;
+        };
+        //endregion
     }
 
     override fun initData() {
-
+        //region init payment suggestion data
+        val sauceItemList: MutableList<TemporarySauceItem> =
+            (temporaryStyleVM.initSauceItem() as List<TemporarySauceItem>).toMutableList();
+        temporarySauceAdapter.submitList(sauceItemList);
+        //endregion
     }
 
     override fun initAction() {
 
     }
 
+    override fun getBack() {
+        onFragmentBackPressed()
+    }
+
 
 }
+
+data class TemporarySauceItem(val name: String)
