fe49b23f4a8928bc6173b401c2af4e0877402a73
Xuan Hung <nxhung16102001@gmail.com>
2022-01-05 10:52:34 +0700

    update - discount automatic

--- app/src/main/java/com/hanheldpos/ui/screens/discount/discount_type/automatic/DiscountAutomaticFragment.kt
+++ app/src/main/java/com/hanheldpos/ui/screens/discount/discount_type/automatic/DiscountAutomaticFragment.kt
@@ -1,14 +1,24 @@
 package com.hanheldpos.ui.screens.discount.discount_type.automatic
 
 import com.hanheldpos.R
+import com.hanheldpos.data.api.pojo.discount.DiscountItem
 import com.hanheldpos.databinding.FragmentDiscountAutomaticBinding
+import com.hanheldpos.model.cart.CartModel
+import com.hanheldpos.model.product.BaseProductInCart
+import com.hanheldpos.ui.base.adapter.BaseItemClickListener
 import com.hanheldpos.ui.base.fragment.BaseFragment
+import com.hanheldpos.ui.screens.discount.discount_type.discount_code.adapter.DiscountCodeAdapter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 
-class DiscountAutomaticFragment : BaseFragment<FragmentDiscountAutomaticBinding,DiscountAutomaticVM>() , DiscountAutomaticUV {
+class DiscountAutomaticFragment(private val item : BaseProductInCart?, private val cart : CartModel?) : BaseFragment<FragmentDiscountAutomaticBinding,DiscountAutomaticVM>() , DiscountAutomaticUV {
     override fun layoutRes(): Int {
         return R.layout.fragment_discount_automatic;
     }
+
+    private lateinit var discountCodeAdapter: DiscountCodeAdapter;
 
     override fun viewModelClass(): Class<DiscountAutomaticVM> {
         return DiscountAutomaticVM::class.java;
@@ -22,15 +32,30 @@ class DiscountAutomaticFragment : BaseFragment<FragmentDiscountAutomaticBinding,
     }
 
     override fun initView() {
+        discountCodeAdapter =
+            DiscountCodeAdapter(listener = object : BaseItemClickListener<DiscountItem> {
+                override fun onItemClick(adapterPosition: Int, item: DiscountItem) {
 
+                }
+            });
+        binding.listDiscountCode.adapter = discountCodeAdapter;
     }
 
     override fun initData() {
+        GlobalScope.launch(Dispatchers.IO) {
+            viewModel.loadDiscountAutomatic(item,cart);
+        }
+
 
     }
 
     override fun initAction() {
 
+    }
+
+    override fun loadDataDiscountCode(list: List<DiscountItem>) {
+        discountCodeAdapter.submitList(list);
+        discountCodeAdapter.notifyDataSetChanged();
     }
 
 }
--- app/src/main/java/com/hanheldpos/ui/screens/discount/discount_type/automatic/DiscountAutomaticUV.kt
+++ app/src/main/java/com/hanheldpos/ui/screens/discount/discount_type/automatic/DiscountAutomaticUV.kt
@@ -1,6 +1,8 @@
 package com.hanheldpos.ui.screens.discount.discount_type.automatic
 
+import com.hanheldpos.data.api.pojo.discount.DiscountItem
 import com.hanheldpos.ui.base.BaseUserView
 
 interface DiscountAutomaticUV : BaseUserView {
+    fun loadDataDiscountCode(list : List<DiscountItem>);
 }
