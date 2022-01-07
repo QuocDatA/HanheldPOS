a5c9c3dc55a391b20093ee30421960cb9f753cac
KSB-tqk <linh05602@gmail.com>
2022-01-07 17:28:11 +0700

    [REFACTOR] - Refactor Menu Category

--- app/src/main/java/com/hanheldpos/model/home/order/menu/CategoryModeViewType.kt
+++ app/src/main/java/com/hanheldpos/model/home/order/menu/CategoryModeViewType.kt
@@ -2,10 +2,6 @@ package com.hanheldpos.model.home.order.menu
 
 enum class MenuModeViewType(val value: Int) {
     Menu(1),
-    DirectionDisableUpDown(2),
-    DirectionEnableDown(3),
-    DirectionEnableUp(4),
-    DirectionEnableUpDown(5),
     Empty(6);
     /*
             * 0 : Disable All
