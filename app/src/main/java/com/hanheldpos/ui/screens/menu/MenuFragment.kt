827ffc031f3c0b08f2c50787fe9c0f8b6786fb59
KSB-tqk <linh05602@gmail.com>
2022-01-10 22:32:25 +0700

    [DEV] - Add Current Drawer in Report in Menu Option

--- app/src/main/java/com/hanheldpos/ui/screens/menu/MenuFragment.kt
+++ app/src/main/java/com/hanheldpos/ui/screens/menu/MenuFragment.kt
@@ -14,6 +14,7 @@ import com.hanheldpos.ui.base.adapter.BaseItemClickListener
 import com.hanheldpos.ui.base.dialog.AppAlertDialog
 import com.hanheldpos.ui.base.fragment.BaseFragment
 import com.hanheldpos.ui.screens.menu.adapter.MenuAdapter
+import com.hanheldpos.ui.screens.menu.option.report.ReportFragment
 import com.hanheldpos.ui.screens.pincode.PinCodeActivity
 import com.hanheldpos.ui.screens.welcome.WelcomeActivity
 
@@ -85,7 +86,7 @@ class MenuFragment : BaseFragment<FragmentMenuBinding, MenuVM>(), MenuUV {
         when (option.type) {
             NavBarOptionType.ORDERS -> TODO()
             NavBarOptionType.TRANSACTIONS -> TODO()
-            NavBarOptionType.REPORTS -> TODO()
+            NavBarOptionType.REPORTS -> navigateToReport()
             NavBarOptionType.CUSTOMER -> TODO()
             NavBarOptionType.ORDER_HISTORY -> TODO()
             NavBarOptionType.SETTINGS -> TODO()
@@ -97,6 +98,10 @@ class MenuFragment : BaseFragment<FragmentMenuBinding, MenuVM>(), MenuUV {
             )
             NavBarOptionType.RESET_SYSTEM -> TODO()
         }
+    }
+
+    fun navigateToReport() {
+        navigator.goTo(ReportFragment())
     }
 
     private fun onLogoutOption(type: LogoutType, title: String?, message: String?) {
---
+++ app/src/main/java/com/hanheldpos/ui/screens/menu/option/report/drawer/CurrentDrawerUV.kt
@@ -0,0 +1,7 @@
+package com.hanheldpos.ui.screens.menu.option.report.drawer
+
+import com.hanheldpos.ui.base.BaseUserView
+
+interface CurrentDrawerUV: BaseUserView {
+    fun getBack()
+}
