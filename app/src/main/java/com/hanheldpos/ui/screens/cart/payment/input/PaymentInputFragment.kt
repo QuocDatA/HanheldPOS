38daa972b587d061b48330e3bbcefb8b6f92731a
KSB-tqk <linh05602@gmail.com>
2022-01-17 14:44:23 +0700

    [REFACTOR] - Refactor payment method flow

--- app/src/main/java/com/hanheldpos/ui/screens/cart/payment/input/PaymentInputFragment.kt
+++ app/src/main/java/com/hanheldpos/ui/screens/cart/payment/input/PaymentInputFragment.kt
@@ -1,11 +1,11 @@
 package com.hanheldpos.ui.screens.cart.payment.input
 
 import android.annotation.SuppressLint
-import android.view.View
 import com.hanheldpos.R
 import com.hanheldpos.data.api.pojo.payment.PaymentMethodResp
 import com.hanheldpos.databinding.FragmentPaymentInputBinding
 import com.hanheldpos.extension.toNiceString
+import com.hanheldpos.model.cart.payment.PaymentMethodType
 import com.hanheldpos.model.keyboard.KeyBoardType
 import com.hanheldpos.ui.base.fragment.BaseFragment
 import com.hanheldpos.ui.input.KeyBoardVM
@@ -13,7 +13,7 @@ import com.hanheldpos.ui.input.KeyBoardVM
 class PaymentInputFragment(
     private val payable: Double,
     private val paymentMethod: PaymentMethodResp,
-    private val listener: PaymentInputListener? = null
+    private val listener: PaymentInputListener? = null,
 ) :
     BaseFragment<FragmentPaymentInputBinding, PaymentInputVM>(), PaymentInputUV {
 
@@ -55,7 +55,7 @@ class PaymentInputFragment(
                 binding.keyBoardContainer.textPad.isCapLock = keyBoardVM.isCapLock
             }
         });
-        binding.paymentInputTitle.setText(paymentMethod.Title + " (Amount Due " + payable.toNiceString() + ")")
+
     }
 
     override fun onCancel() {
@@ -69,11 +69,27 @@ class PaymentInputFragment(
     }
 
     override fun initData() {
-        keyBoardVM.input.value = payable.toNiceString();
-        keyBoardVM.keyBoardType = KeyBoardType.Number
+        //region setup keyboard
+        keyBoardVM.keyBoardType = viewModel.setUpKeyboard(paymentMethod)
+        print(keyBoardVM.keyBoardType)
+        if (keyBoardVM.keyBoardType == KeyBoardType.NumberOnly) {
+            keyBoardVM.input.value = payable.toNiceString();
+            binding.paymentInputTitle.setText(paymentMethod.Title + " (Amount Due " + payable.toNiceString() + ")")
+            if (paymentMethod.PaymentMethodType == PaymentMethodType.CASH.value) {
+                keyBoardVM.input.value = ""
+                binding.numberPaymentInput.setHint("Enter amount")
+            }
+        } else {
+            keyBoardVM.input.value = ""
+            binding.paymentInputTitle.setText("Input Card Number")
+            binding.numberPaymentInput.setHint("Card Number")
+
+        }
         binding.keyBoardContainer.textPad.isCapLock = keyBoardVM.isCapLock
         binding.keyBoardContainer.keyBoardType = keyBoardVM.keyBoardType
         binding.keyBoardContainer.numberPad.keyBoardType = keyBoardVM.keyBoardType
+        //endregion
+
     }
 
     override fun initAction() {
@@ -83,12 +99,12 @@ class PaymentInputFragment(
         fun getInstance(
             listener: PaymentInputFragment.PaymentInputListener? = null,
             paymentMethod: PaymentMethodResp,
-            payable: Double
+            payable: Double,
         ): PaymentInputFragment {
             return PaymentInputFragment(
                 listener = listener,
                 paymentMethod = paymentMethod,
-                payable = payable
+                payable = payable,
             ).apply {
 
             };
