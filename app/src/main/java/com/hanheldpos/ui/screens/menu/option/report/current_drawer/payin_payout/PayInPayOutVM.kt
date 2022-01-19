7502b7c6788090f43f28b1c13032f4ad43e29073
Xuan Hung <nxhung16102001@gmail.com>
2022-01-19 11:27:59 +0700

    update - ui payin payout

--- app/src/main/java/com/hanheldpos/ui/screens/menu/option/report/current_drawer/payin_payout/PayInPayOutVM.kt
+++ app/src/main/java/com/hanheldpos/ui/screens/menu/option/report/current_drawer/payin_payout/PayInPayOutVM.kt
@@ -1,26 +1,43 @@
 package com.hanheldpos.ui.screens.menu.option.report.current_drawer.payin_payout
 
+
 import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import com.hanheldpos.ui.base.viewmodel.BaseUiViewModel
 
 class PayInPayOutVM : BaseUiViewModel<PayInPayOutUV>() {
     val amountString = MutableLiveData<String>();
     var amount : Double? = null;
-
     var description = MutableLiveData<String>();
 
-    fun initLifeCycle(owner: LifecycleOwner) {
-        amountString.observe(owner,{
-            amount = it.replace(",","").toDouble();
-        });
+    val isValid = MediatorLiveData<Boolean>().apply {
+        value = false;
+    };
+
+    init {
+
     }
 
-    fun isValid() : Boolean {
-        amount?:return false;
-        description.value?:return false;
-        return amount!! > 0.0 && !description.value?.trim().isNullOrEmpty();
+    fun initLifeCycle(owner: LifecycleOwner) {
+        isValid.addSource(amountString) {
+            amount = it.replace(",","").toDouble();
+            isValid.value = if (amount == null || description.value == null) {
+                false
+            } else {
+                amount!! > 0.0 && !description.value?.trim().isNullOrEmpty();
+            }
+        }
+        isValid.addSource(description) {
+            isValid.value = if (amount == null || description.value == null) {
+                false
+            } else {
+                amount!! > 0.0 && !description.value?.trim().isNullOrEmpty();
+            }
+        }
     }
+
+
 
     fun backPress() {
         uiCallback?.getBack()
