9e073a5158d859a1c006e7284b55cd05982d44d2
KSB-tqk <linh05602@gmail.com>
2022-01-26 19:10:10 +0700

    [REFACTOR] - Refactor UI PaymentInput

--- app/src/main/java/com/hanheldpos/ui/screens/cart/payment/input/PaymentInputVM.kt
+++ app/src/main/java/com/hanheldpos/ui/screens/cart/payment/input/PaymentInputVM.kt
@@ -1,39 +1,53 @@
 package com.hanheldpos.ui.screens.cart.payment.input
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.MutableLiveData
 import com.hanheldpos.data.api.pojo.payment.PaymentMethodResp
+import com.hanheldpos.extension.toNiceString
 import com.hanheldpos.model.cart.payment.PaymentAppyTo
 import com.hanheldpos.model.cart.payment.PaymentMethodType
 import com.hanheldpos.model.keyboard.KeyBoardType
 import com.hanheldpos.ui.base.viewmodel.BaseUiViewModel
 
 class PaymentInputVM : BaseUiViewModel<PaymentInputUV>() {
-    fun onComplete(){
+    val paymentMethodTitle = MutableLiveData<String>()
+
+    fun onComplete() {
         uiCallback?.onComplete();
     }
-    fun onCancel(){
+
+    fun onCancel() {
         uiCallback?.onCancel();
     }
-    fun setUpKeyboard(paymentMethodResp: PaymentMethodResp): KeyBoardType{
+
+    fun clearInput() {
+        uiCallback?.clearInput()
+    }
+
+    fun setUpKeyboard(paymentMethod: PaymentMethodResp, payable: Double): KeyBoardType {
         val keyBoardType: KeyBoardType
-        when(paymentMethodResp.PaymentMethodType){
+        when (paymentMethod.PaymentMethodType) {
             PaymentMethodType.CASH.value -> {
                 keyBoardType = KeyBoardType.NumberOnly
+                paymentMethodTitle.postValue(paymentMethod.Title)
             }
             PaymentMethodType.WALLET.value -> {
                 keyBoardType = KeyBoardType.Text
+                paymentMethodTitle.postValue("Input Card Number")
+                uiCallback?.clearInput()
             }
             PaymentMethodType.GIFT_CARD.value -> {
                 keyBoardType = KeyBoardType.Text
+                paymentMethodTitle.postValue("Input Card Number")
+                uiCallback?.clearInput()
             }
             else -> {
-                keyBoardType = when(paymentMethodResp.ApplyToId) {
-                    PaymentAppyTo.SODEXO.value -> {
-                        KeyBoardType.Text
-                    }
-                    else -> {
-                        KeyBoardType.NumberOnly
-                    }
+                if (paymentMethod.PaymentMethodType == PaymentAppyTo.SODEXO.value) {
+                    keyBoardType = KeyBoardType.Text
+                    paymentMethodTitle.postValue("Input Card Number")
+                    uiCallback?.clearInput()
+                } else {
+                    keyBoardType = KeyBoardType.NumberOnly
+                    paymentMethodTitle.postValue(paymentMethod.Title + " (Amount Due " + payable.toNiceString() + ")")
                 }
             }
         }
