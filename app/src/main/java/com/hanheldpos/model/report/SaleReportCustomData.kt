52d31010b69e407acd736f60e8694eb0ffde102c
Xuan Hung <nxhung16102001@gmail.com>
2022-02-07 17:47:10 +0700

    update - date select fast

--- app/src/main/java/com/hanheldpos/model/report/SaleReportCustomData.kt
+++ app/src/main/java/com/hanheldpos/model/report/SaleReportCustomData.kt
@@ -3,12 +3,12 @@ package com.hanheldpos.model.report
 import java.util.*
 
 data class SaleReportCustomData(
-    val startDay: Date?,
-    val endDay: Date?,
-    val isAllDay: Boolean,
-    val startTime: String,
-    val endTime: String,
-    val isAllDevice: Boolean,
-    val isCurrentDrawer: Boolean
+    var startDay: Date?,
+    var endDay: Date?,
+    var isAllDay: Boolean,
+    var startTime: String,
+    var endTime: String,
+    var isAllDevice: Boolean,
+    var isCurrentDrawer: Boolean
 ) {
 }
