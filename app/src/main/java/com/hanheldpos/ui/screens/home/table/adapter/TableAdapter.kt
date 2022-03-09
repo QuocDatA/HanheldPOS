5d7da1e49263012450e7f3537e25f59f9b69ef94
nxhung.hit <nxhung16102001@gmail.com>
2022-03-09 13:40:36 +0700

    fix - calculate time order on table

--- app/src/main/java/com/hanheldpos/ui/screens/home/table/adapter/TableAdapter.kt
+++ app/src/main/java/com/hanheldpos/ui/screens/home/table/adapter/TableAdapter.kt
@@ -123,7 +123,7 @@ class TableAdapter(
                                 view.timeCount.text = String.format("%02d:%02d",
                                     TimeUnit.MILLISECONDS.toHours(time),
                                     TimeUnit.MILLISECONDS.toMinutes(time) -
-                                            TimeUnit.MINUTES.toMinutes(TimeUnit.MILLISECONDS.toHours(time))
+                                            TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(time))
                                 );
 
                             }
