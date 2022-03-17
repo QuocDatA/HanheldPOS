1778d44b0da38afbc0db2734e95d4c6baf200a79
nxhung.hit <nxhung16102001@gmail.com>
2022-03-17 14:38:08 +0700

    update - payment gift card

---
+++ app/src/main/java/com/hanheldpos/data/api/pojo/payment/GiftCardResp.kt
@@ -0,0 +1,23 @@
+package com.hanheldpos.data.api.pojo.payment
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class CardNumberResp(
+    val Active: Int,
+    val Balance: Double,
+    val CardNumber: String,
+    val CreateDate: String,
+    val Discount: Double,
+    val ExpDate: String,
+    val LinePrice: Double,
+    val LocationGuid: String?,
+    val Payable: Double,
+    val Receivable: Double,
+    val UserGuid: String,
+    val Visible: Int,
+    val _id: String,
+    val _key: Int,
+    val _rev: String
+) : Parcelable
