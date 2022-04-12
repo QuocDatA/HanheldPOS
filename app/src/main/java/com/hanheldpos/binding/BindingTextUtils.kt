f308d4770d2b13f7b50eff84a3d992ca07286ea3
nxhung.hit <nxhung16102001@gmail.com>
2022-04-12 16:22:38 +0700

    fix - end drawer

--- app/src/main/java/com/hanheldpos/binding/BindingTextUtils.kt
+++ app/src/main/java/com/hanheldpos/binding/BindingTextUtils.kt
@@ -1,108 +1,26 @@
 package com.hanheldpos.binding
 
 import android.graphics.Paint
+import android.widget.EditText
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.hanheldpos.utils.NumberTextWatcher
 import java.text.DecimalFormat
 import java.text.DecimalFormatSymbols
 
 @BindingAdapter("priceView")
 fun setPriceView(view: TextView?, price: Double?) {
     if (view == null || price == null) return
-    val dfSymbols = DecimalFormatSymbols()
-    dfSymbols.decimalSeparator = '.'
-    dfSymbols.groupingSeparator = ','
-    val df = DecimalFormat("###", dfSymbols)
-    df.groupingSize = 3
-    df.isGroupingUsed = true
-    val rs = df.format(price);
-    view.text = rs
+
+    view.addTextChangedListener(NumberTextWatcher(view as EditText))
 }
 
 @BindingAdapter("pricePlusView")
 fun setPricePlusView(view: TextView?, price: Double?) {
     if (view == null || price == null) return
-    val dfSymbols = DecimalFormatSymbols()
-    dfSymbols.decimalSeparator = '.'
-    dfSymbols.groupingSeparator = ','
-    val df = DecimalFormat("###", dfSymbols)
-    df.groupingSize = 3
-    df.isGroupingUsed = true
-    val rs = df.format(price);
-    view.text = "+$rs"
+    view.addTextChangedListener(NumberTextWatcher(view as EditText))
 }
 
-@BindingAdapter("basePriceView")
-fun setBasePriceView(view: TextView?, price: Double?) {
-    if (view == null || price == null) return
-    val dfSymbols = DecimalFormatSymbols()
-    dfSymbols.decimalSeparator = '.'
-    dfSymbols.groupingSeparator = ','
-    val df = DecimalFormat("###", dfSymbols)
-    df.groupingSize = 3
-    df.isGroupingUsed = true
-    val rs = df.format(price);
-    view.text = "(Base price: $rs)"
-}
-
-@BindingAdapter("addPriceView")
-fun setAddPriceView(view: TextView?, price: Double?) {
-    if (view == null || price == null) return
-
-    val dfSymbols = DecimalFormatSymbols()
-    dfSymbols.decimalSeparator = '.'
-    dfSymbols.groupingSeparator = ','
-    val df = DecimalFormat("###", dfSymbols)
-    df.groupingSize = 3
-    df.isGroupingUsed = true
-    val rs = df.format(price);
-    view.text = "+$rs"
-}
-
-@BindingAdapter("priceDisc")
-fun setDiscPriceView(view: TextView?, price: Double?) {
-    if (view == null || price == null) return
-
-    val dfSymbols = DecimalFormatSymbols()
-    dfSymbols.decimalSeparator = '.'
-    dfSymbols.groupingSeparator = ','
-    val df = DecimalFormat("###", dfSymbols)
-    df.groupingSize = 3
-    df.isGroupingUsed = true
-    val rs = df.format(price);
-
-    view.text = "($rs)"
-}
-
-@BindingAdapter("priceDiscMinusRound")
-fun setDiscPriceMinusRoundView(view: TextView?, price: Double?) {
-    if (view == null || price == null) return
-
-    val dfSymbols = DecimalFormatSymbols()
-    dfSymbols.decimalSeparator = '.'
-    dfSymbols.groupingSeparator = ','
-    val df = DecimalFormat("###", dfSymbols)
-    df.groupingSize = 3
-    df.isGroupingUsed = true
-    val rs = df.format(price);
-
-    view.text = "(-$rs)"
-}
-
-@BindingAdapter("priceDiscMinus")
-fun setDiscPriceMinusView(view: TextView?, price: Double?) {
-    if (view == null || price == null) return
-
-    val dfSymbols = DecimalFormatSymbols()
-    dfSymbols.decimalSeparator = '.'
-    dfSymbols.groupingSeparator = ','
-    val df = DecimalFormat("###", dfSymbols)
-    df.groupingSize = 3
-    df.isGroupingUsed = true
-    val rs = df.format(price);
-
-    view.text = "-$rs"
-}
 
 @BindingAdapter("bindStrike")
 fun setbindStrike(textView: TextView, isBind: Boolean?) {
