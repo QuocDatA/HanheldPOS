09d21d3f51f892e2b79a5cb2ef02e24a86cdbdf8
nxhung.hit <nxhung16102001@gmail.com>
2022-04-13 17:23:37 +0700

    temp - printer

---
+++ pos_printer/src/main/java/com/handheld/pos_printer/BasePrintManager.kt
@@ -0,0 +1,24 @@
+package com.handheld.pos_printer
+
+import android.graphics.Bitmap
+
+abstract class BasePrintManager {
+    abstract fun connect()
+    abstract fun disconnect()
+    abstract fun setupPage(width : Int, height : Int)
+    abstract fun drawText(data: String, bold: Boolean = false, size : FontSize = FontSize.Small)
+    abstract fun drawBitmap(bitmap : Bitmap, align : BitmapAlign)
+    abstract fun drawBarcode()
+
+    enum class FontSize {
+        Small,
+        Medium,
+        Large,
+    }
+
+    enum class BitmapAlign {
+        Right,
+        Left,
+        Center,
+    }
+}
