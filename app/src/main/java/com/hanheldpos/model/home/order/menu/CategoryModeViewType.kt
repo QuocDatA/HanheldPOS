ee351bf9ac0586b38f324e512023191d6785d294
Khanh Tran <khanhsbtqk@gmail.com>
2022-04-22 15:20:27 +0700

    [REF] - fix ui category menu

--- app/src/main/java/com/hanheldpos/model/home/order/menu/CategoryModeViewType.kt
+++ app/src/main/java/com/hanheldpos/model/home/order/menu/CategoryModeViewType.kt
@@ -2,7 +2,9 @@ package com.hanheldpos.model.home.order.menu
 
 enum class MenuModeViewType(val value: Int) {
     Menu(1),
-    PrevButton(2),
-    NextButton(3),
+    PrevButtonEnable(2),
+    NextButtonEnable(3),
+    BothButtonEnable(4),
+    NoneButtonEnable(5),
     Empty(6);
 }
