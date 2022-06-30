e52ec7e9d1b8d4870795cde0ea3ff57ec02590dc
Khanh Tran <khanhsbtqk@gmail.com>
2022-06-30 17:25:58 +0700

    [Feat] - hardware detail on save

--- app/src/main/java/com/hanheldpos/ui/screens/menu/settings/hardware/hardware_detail/adapter/HardwareConnectionAdapter.kt
+++ app/src/main/java/com/hanheldpos/ui/screens/menu/settings/hardware/hardware_detail/adapter/HardwareConnectionAdapter.kt
@@ -1,8 +1,13 @@
 package com.hanheldpos.ui.screens.menu.settings.hardware.hardware_detail.adapter
 
+import android.text.Editable
 import android.text.InputFilter
 import android.text.Spanned
+import android.text.TextWatcher
+import androidx.core.graphics.blue
+import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.DiffUtil
+import com.google.android.material.textfield.TextInputEditText
 import com.hanheldpos.R
 import com.hanheldpos.data.api.pojo.setting.hardware.HardwareConnection
 import com.hanheldpos.databinding.ItemHardwareConnectionBinding
@@ -11,14 +16,25 @@ import com.hanheldpos.ui.base.adapter.BaseBindingViewHolder
 import com.hanheldpos.ui.base.adapter.BaseItemClickListener
 import com.hanheldpos.ui.screens.menu.settings.adapter.SettingsOptionAdapter
 
-class HardwareConnectionAdapter(private val listener: BaseItemClickListener<HardwareConnection>) :
+class HardwareConnectionAdapter(
+
+    private val listener: HardwareConnectionCallBack
+) :
     BaseBindingListAdapter<HardwareConnection>(DiffCallBack()) {
 
     private var selectedItem: SettingsOptionAdapter.SelectedItem =
-        SettingsOptionAdapter.SelectedItem(-1)
+        SettingsOptionAdapter.SelectedItem(0)
+
+    private var defaultSelectedPosition = -1
 
     override fun getItemViewType(position: Int): Int {
         return R.layout.item_hardware_connection
+    }
+
+    override fun submitList(list: MutableList<HardwareConnection>?) {
+        selectedItem.value = list?.indexOfFirst { it.isChecked == true } ?: 0
+        defaultSelectedPosition = selectedItem.value
+        super.submitList(list)
     }
 
     override fun onBindViewHolder(
@@ -26,9 +42,6 @@ class HardwareConnectionAdapter(private val listener: BaseItemClickListener<Hard
         position: Int
     ) {
         val item = getItem(position)
-        if (selectedItem.value == -1 && item.isChecked == true) {
-            selectedItem.value = position
-        }
         holder.bindItem(item)
         val binding = holder.binding as ItemHardwareConnectionBinding
         binding.isSelected = (position == selectedItem.value)
@@ -38,12 +51,41 @@ class HardwareConnectionAdapter(private val listener: BaseItemClickListener<Hard
                 selectedItem.value = position
                 notifyItemChanged(position)
                 binding.isSelected = (position == selectedItem.value)
-                item.isChecked = !item.isChecked!!
+                listener.onItemClick(position, item)
+            }
+            if (selectedItem.value == position) {
+                checkStatusSave(holder.absoluteAdapterPosition, binding.editTextIPAddress)
                 listener.onItemClick(position, item)
             }
             isChecked = selectedItem.value == position
+
         }
-        binding.editTextIPAddress.hint = item.port.toString()
+        binding.editTextIPAddress.setText(item.port.toString())
+        binding.editTextIPAddress.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                checkStatusSave(holder.absoluteAdapterPosition, binding.editTextIPAddress)
+            }
+        })
+    }
+
+    fun checkStatusSave(position: Int, editor: TextInputEditText) {
+        if ((selectedItem.value == defaultSelectedPosition && editor.text?.isNotBlank() == true && editor.text.toString() != getItem(
+                position
+            ).port) || selectedItem.value != defaultSelectedPosition
+        ) {
+            listener.onSaveEnable()
+
+        } else {
+            listener.onSaveDisable()
+        }
     }
 
     private class DiffCallBack : DiffUtil.ItemCallback<HardwareConnection>() {
@@ -61,5 +103,11 @@ class HardwareConnectionAdapter(private val listener: BaseItemClickListener<Hard
             return oldItem == newItem
         }
 
+    }
+
+    interface HardwareConnectionCallBack {
+        fun onItemClick(position: Int, item: HardwareConnection)
+        fun onSaveEnable()
+        fun onSaveDisable()
     }
 }
